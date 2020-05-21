3648c5fb9130311e1ae2c3246d58e9033acdd522
hiddenfloret <vladut.bratulescu@gmail.com>
2020-05-21 18:31:35 +0200

    SYPR Exercise 2 updated again

--- SYPR/sypr-uebungen/aufgabe2/bubblesort.c
+++ SYPR/sypr-uebungen/aufgabe2/bubblesort.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
     srand((unsigned int) time(NULL));
 
     int n = atoi(argv[1]);
-
-    if (argc != 2 || n == 0)
+    unsigned long numDigits;
+    
+    if (argc != 2)
     {
         printf("Aufruf: ./bubblesort Anzahl\n");
+        return 1;
+    }
+
+    if (n == 0) 
+    {
+        numDigits = 1;
+    }
+    else
+    {
+        // Formel fuer Berechnung Anzahl Ziffern
+        numDigits = floor(log10(abs(n))) + 1;
+    }
+
+    // Die erste Bedingung vergleicht die Anzahl Ziffern von n mit der Anzahl Zeichen von argv[1]
+    // Somit werden Eingaben wie 5-1, 5.1, 5junk, usw. nicht erlaubt
+    if ((numDigits != strlen(argv[1]) && n >= 0)) {
+        printf("NumberFormatException");
         return 1;
     }
 
