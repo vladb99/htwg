7132c5477f3b3c4a502f782a10bf2ee6b2468ad9
hiddenfloret <vladut.bratulescu@gmail.com>
2020-05-22 09:16:56 +0200

    SYPR Exercise 2 Extra programs added

---
+++ SYPR/sypr-uebungen/aufgabe2/for_iterative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Aufruf: ./for_iterative Anzahl\n");
+        return 1;
+    }
+
+    srand((unsigned int) time(NULL));
+    int n = atoi(argv[1]);
+    int *a = (int*) calloc((size_t) n, sizeof (int));
+
+    if (a == NULL)
+    {
+        printf("Speicherreservierung fehlgeschlagen!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        int r = rand() % n;
+        a[i] = r;
+    }
+
+    free(a);
+    return 0;
+}
