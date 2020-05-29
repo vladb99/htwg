3a984a2523fc44e5f3e2769948c9a6c25ab87879
hiddenfloret <vladut.bratulescu@gmail.com>
2020-05-29 11:35:45 +0200

    SYPR Exercise 3 update

--- SYPR/sypr-uebungen/aufgabe3/stringsort.c
+++ SYPR/sypr-uebungen/aufgabe3/stringsort.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-void bubblesort(char *list[]);
+void bubblesort(int size, char *list[]);
 
 int main(int argc, char *argv[])
 {
@@ -16,37 +16,86 @@ int main(int argc, char *argv[])
 
     srand((unsigned int) time(NULL));
     int n = atoi(argv[1]);
+    int m = strlen(argv[1]) + 1;
 
     if (n < 1) {
-        printf("Anzahl muss midestens 1 sein");
+        printf("Anzahl muss mindestens 1 sein");
         return 1;
     }
 
-    char *a[n];
-
-    for (int i = 0; i < 6; ++i) 
+    char **arrayString = (char**) malloc(n * sizeof (char*));
+    if (arrayString == NULL)
     {
-        a[i] = (char*) malloc(strlen(argv[1]) * sizeof(char));
+        printf("Speicherallokierungsfehler\n");
+        return 1;
+    }
 
-        if (a[i] == NULL)
+    printf("Unsortiertes Feld:\n");
+    for (int i = 0; i < n; ++i) 
+    {
+        int r = rand() % n;
+
+        arrayString[i] = (char*) malloc(m * sizeof (char));
+        if (arrayString[i] == NULL)
         {
-            printf("Speicherreservierung fehlgeschlagen!\n");
+            printf("Speicherallokierungsfehler\n");
+            for (int j = 0; j < i; ++j)
+            {
+                free(arrayString[j]);
+            }
+            free(arrayString);
             return 1;
+        }
+
+        sprintf(arrayString[i], "%d", r);
+        printf("%s ", arrayString[i]);
+    }
+
+    bubblesort(n, arrayString);
+    
+    printf("\nSortiertes Feld:\n");
+    char *sb = (char*) malloc(n * m * sizeof(char));
+    if (sb == NULL)
+    {
+        printf("Speicherallokierungsfehler\n");
+        return 1;
+    }
+    strcat(sb, arrayString[0]);
+    for (int i = 1; i < n; ++i)
+    {
+        if (strcmp(arrayString[i], arrayString[i - 1]) == 0)
+        {
+            strcat(sb, "*");
+        }
+        else
+        {
+            strcat(sb, " ");
+            strcat(sb, arrayString[i]);
         }
     }
 
-    printf("Unsortiertes Feld:");
+    printf("%s", sb);
 
+    for (int i = 0; i < n; ++i)
+    {
+        free(arrayString[i]);
+    }
 
-
-
-
-
-    free(a); // maybe it needs to be iterative -> all strings must be freed
+    free(sb);
+    free(arrayString);
     return 0;
 }
 
-void bubblesort(char *list[])
+void bubblesort(int size, char *list[])
 {
-
+    for (int i = size; i > 1; --i) {
+        for (int j = 0; j < i - 1; ++j) {
+            if (strcmp(list[j], list[j + 1]) > 0) {
+                char *tmp = list[j + 1];
+                list[j + 1] = list[j];
+                list[j] = tmp;
+            }
+        }
+    }
 }
+
