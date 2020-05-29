84b05df6e83fd2873cf19ee6345e29701dbc28cd
hiddenfloret <vladut.bratulescu@gmail.com>
2020-05-29 14:18:41 +0200

    SYPR Exercise 3 refactor

---
+++ SYPR/sypr-uebungen/aufgabe3/stringsort-optimiert.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void bubblesort(int size, char *list[]);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Aufruf: %s Anzahl\n", *argv);
+        return 1;
+    }
+
+    srand((unsigned int) time(NULL));
+    int n = atoi(argv[1]);
+    int m = strlen(argv[1]) + 1;
+
+    if (n < 1) {
+        printf("Anzahl muss mindestens 1 sein\n");
+        return 1;
+    }
+
+    //char **arrayString = (char**) malloc(n * sizeof (char*));
+    char *arrayString = (char*) malloc(n * m * sizeof (char));
+    if (arrayString == NULL)
+    {
+        printf("Speicherallokierungsfehler\n");
+        return 1;
+    }
+
+    printf("Unsortiertes Feld:\n");
+    for (int i = 0; i < n; ++i)
+    {
+        int r = rand() % n;
+        sprintf(&arrayString[i * m], "%d", r);
+        for (int j = 0; j < m; ++j)
+        {
+            printf("%c", arrayString[i * m + j]);
+        }
+        printf(" ");
+    }
+    printf("\n");
+    /*
+    bubblesort(n, arrayString);
+    
+    printf("\nSortiertes Feld:\n");
+    char *sb = (char*) malloc(n * m * sizeof(char));
+    if (sb == NULL)
+    {
+        printf("Speicherallokierungsfehler\n");
+        return 1;
+    }
+    strcpy(sb, arrayString[0]);
+    for (int i = 1; i < n; ++i)
+    {
+        if (strcmp(arrayString[i], arrayString[i - 1]) == 0)
+        {
+            strcat(sb, "*");
+        }
+        else
+        {
+            strcat(sb, " ");
+            strcat(sb, arrayString[i]);
+        }
+    }
+
+    printf("%s\n", sb);
+
+    for (int i = 0; i < n; ++i)
+    {
+        free(arrayString[i]);
+    }
+
+    free(sb);
+    */
+    free(arrayString);
+    return 0;
+}
+
+void bubblesort(int size, char *list[])
+{
+    for (int i = size; i > 1; --i) {
+        for (int j = 0; j < i - 1; ++j) {
+            if (strcmp(list[j], list[j + 1]) > 0) {
+                char *tmp = list[j + 1];
+                list[j + 1] = list[j];
+                list[j] = tmp;
+            }
+        }
+    }
+}
