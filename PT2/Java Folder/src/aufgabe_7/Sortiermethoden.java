df0be70fe485dd5f5c0a4f288d31419400a76bfc
hiddenfloret <vladut.bratulescu@gmail.com>
2020-06-15 18:30:12 +0200

    PT2 Exercise 7 refactor

--- PT2/Java Folder/src/aufgabe_7/Sortiermethoden.java
+++ PT2/Java Folder/src/aufgabe_7/Sortiermethoden.java
@@ -2,40 +2,85 @@ package aufgabe_7;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class Sortiermethoden {
+    private static final int N = 100;
+
     public static void main(String[] args) {
         List<Integer> a = new LinkedList<>();
-        a.add(7);
-        a.add(2);
-        a.add(1);
-        a.add(2);
-        a.add(8);
-        a.add(9);
-        a.add(4);
-        a.add(3);
-        a.add(6);
-        System.out.println(a);
-        hybrid_quicksort(a);
-        System.out.println(a);
-    }
-
-    private static <T extends Comparable<? super T>> void hybrid_quicksort(List<T> list)
-    {
-        hybrid_quicksort(list, 0, list.size() - 1);
-    }
-
-    private static <T extends Comparable<? super T>> void hybrid_quicksort(List<T> list, int li, int re)
-    {
-        if (re > li)
+        Random r = new Random();
+        for (int i = 0; i < 1000; ++i)
         {
-            int i = partition(list, li, re);
-            hybrid_quicksort(list, li, i-1);
-            hybrid_quicksort(list, i+1, re);
+            a.add(r.nextInt(200));
+        }
+
+        System.out.println(a);
+        hybrid_quicksort(a, true);
+        System.out.println(a);
+    }
+
+    private static <T extends Comparable<? super T>> void hybrid_quicksort(List<T> list, boolean threeMedStrat)
+    {
+        hybrid_quicksort(list, 0, list.size() - 1, threeMedStrat);
+    }
+
+    private static <T extends Comparable<? super T>> void insertionSort(List<T> list)
+    {
+        insertionSort(list, 0, list.size() - 1);
+    }
+
+    private static <T extends Comparable<? super T>> void hybrid_quicksort(List<T> list, int li, int re, boolean threeMedStrat)
+    {
+        /*if (re > li)
+        {
+            if (re - li < N)
+            {
+                insertionSort(list, li, re);
+            }
+            else
+            {
+                int i = partition(list, li, re);
+                hybrid_quicksort(list, li, i-1);
+                hybrid_quicksort(list, i+1, re);
+            }
+        }
+         */
+        while (re > li)
+        {
+            if (re - li < N)
+            {
+                insertionSort(list, li, re);
+                break;
+            }
+            else
+            {
+                /*
+                int i = partition(list, li, re);
+                hybrid_quicksort(list, li, i-1);
+                li = i + 1;
+                 */
+                int i = partition(list, li, re, threeMedStrat);
+                if (i-li < re-i) {
+                    hybrid_quicksort(list, li, i-1, threeMedStrat);
+                    li = i+1;
+                } else {
+                    hybrid_quicksort(list, i+1, re, threeMedStrat);
+                    re = i-1;
+                }
+            }
         }
     }
 
-    private static <T extends Comparable<? super T>> int partition(List<T> list, int li, int re) {
+    private static <T extends Comparable<? super T>> int partition(List<T> list, int li, int re, boolean threeMedStrat)
+    {
+        if (threeMedStrat)
+        {
+            int mid = (li + re) / 2;
+            getMedian(list, li, re, mid);
+            swap(list, re, mid);
+        }
+
         T v = list.get(re);
         int i = li-1;
         int j = re;
@@ -51,14 +96,42 @@ public class Sortiermethoden {
         return i;
     }
 
-    public static <T> void swap(List<T> list, int i, int j) {
+    private static <T extends Comparable<? super T>> void getMedian(List<T> list, int li, int re, int mid)
+    {
+        T liElm = list.get(li);
+        T reElm = list.get(re);
+        T midElm = list.get(mid);
+
+        if (reElm.compareTo(liElm) < 0)
+        {
+            swap(list, li, re);
+        }
+        if (midElm.compareTo(liElm) < 0)
+        {
+            swap(list, mid, li);
+        }
+        if (reElm.compareTo(midElm) < 0)
+        {
+            swap(list, re, mid);
+        }
+    }
+
+    private static <T> void swap(List<T> list, int i, int j) {
         T t = list.get(i);
         list.set(i, list.get(j));
         list.set(j, t);
     }
 
-    private static <T extends Comparable<T>> void insertionSort(T[] a, int li, int re)
+    private static <T extends Comparable<? super T>> void insertionSort(List<T> list, int li, int re)
     {
-
+        for (int i = li + 1; i <= re; i++) {
+            T v = list.get(i);
+            int j = i - 1;
+            while (j >= li && list.get(j).compareTo(v) > 0) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+            list.set(j + 1, v);
+        }
     }
 }
