d73be830e2eac7777b8de2d62982c4ee3132ecd2
hiddenfloret <vladut.bratulescu@gmail.com>
2020-10-22 17:01:51 +0200

    ALDA Work on exercise 1

--- S3/ALDA/Hashi/src/aufgabe1/SortedArrayDictionary.java
+++ S3/ALDA/Hashi/src/aufgabe1/SortedArrayDictionary.java
@@ -4,15 +4,44 @@ import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
-public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
+public class SortedArrayDictionary<K extends Comparable<? super K>, V> implements Dictionary<K, V> {
+
+    private static final int DEF_CAPACITY = 16;
+    private int size;
+    private Entry<K, V>[] data;
+
+    @SuppressWarnings("unchecked")
+    public SortedArrayDictionary() {
+        size = 0;
+        data = new Entry[DEF_CAPACITY];
+    }
 
     @Override
     public V insert(K key, V value) {
+        V i = search(key);
+        if (i != null) {
+            V r = i;
+
+        }
+
         return null;
     }
 
     @Override
     public V search(K key) {
+        int li = 0;
+        int re = size - 1;
+
+        while (re >= li) {
+            int m = (li + re) / 2;
+            if (key.compareTo(data[m].getKey()) < 0) {
+                re = m - 1;
+            } else if (key.compareTo(data[m].getKey()) > 0) {
+                li = m + 1;
+            } else {
+                return data[m].getValue();
+            }
+        }
         return null;
     }
 
@@ -23,7 +52,7 @@ public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
