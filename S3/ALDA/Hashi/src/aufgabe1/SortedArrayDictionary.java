61c1909853c7062a9f655be461f9dcddab1c051d
hiddenfloret <vladut.bratulescu@gmail.com>
2020-10-22 15:37:18 +0200

    ALDA Add basic files

---
+++ S3/ALDA/Hashi/src/aufgabe1/SortedArrayDictionary.java
@@ -0,0 +1,43 @@
+package aufgabe1;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class SortedArrayDictionary<K, V> implements Dictionary<K, V> {
+
+    @Override
+    public V insert(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V search(K key) {
+        return null;
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Entry<K, V>> action) {
+
+    }
+
+    @Override
+    public Spliterator<Entry<K, V>> spliterator() {
+        return null;
+    }
+}
