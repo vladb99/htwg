6e5bf2d5b482b589160ed82bbfbfda2ccd12b3f1
hiddenfloret <vladut.bratulescu@gmail.com>
2020-10-23 15:19:02 +0200

    SE work on string representation

--- S3/SE/scala-project/src/test/HelloWorld.scala
+++ S3/SE/scala-project/src/test/HelloWorld.scala
@@ -1,3 +1,8 @@
-package test object HelloWorld {
+package test
 
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        println("Hello World!")
+        println("Goodbye world!")
+    }
 }
