1f02841ec4b0b2f7994af309e6f81cc2ce5b30ad
hiddenfloret <vladut.bratulescu@gmail.com>
2020-11-07 12:14:25 +0100

    BSYS c14 add free_array.c

---
+++ S3/BS/BSCode/c14_memory-api/free_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    int *array = (int*) malloc(100 * sizeof(int));
+    free(array);
+    printf("%d", array[50]);
+    return 0;
+}
