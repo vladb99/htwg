3b2ce83fa736067b266c008fff61b67a8fc534a7
hiddenfloret <vladut.bratulescu@gmail.com>
2020-12-12 11:52:28 +0100

    S3 update repo ADD notes!

---
+++ S3/BS/Homeworks/HW27-Threads-RealAPI/main-race.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "mythreads.h"
+
+int balance = 0;
+pthread_mutex_t lock;
+
+void* worker(void* arg) {
+    pthread_mutex_lock(&lock);
+    balance++;
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    int rc = pthread_mutex_init(&lock, NULL);
+    assert(rc == 0);
+
+    pthread_t p;
+    Pthread_create(&p, NULL, worker, NULL);
+
+    pthread_mutex_lock(&lock);
+    balance++;
+    pthread_mutex_unlock(&lock);
+
+    Pthread_join(p, NULL);
+    return 0;
+}
