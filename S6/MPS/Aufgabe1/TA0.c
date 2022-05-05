4da5d92cea62e3bda5f0eb46c075b5f193d3bc4d
Vlad Bratulescu <vladut.bratulescu@gmail.com>
2022-05-05 15:50:12 +0200

    927/63 with his solution of checking the buttons. Doesn't work very well when pushing too fast.

--- S6/MPS/Aufgabe1/TA0.c
+++ S6/MPS/Aufgabe1/TA0.c
@@ -11,19 +11,32 @@
 #include "event.h"
 #include "GPIO.h"
 
-#define MAX 4
+#define CNTMAX 4
 #define TIMER_COUNT 25
+#define BTNMAX 2
 
 /*
  * Man soll sich eine geeignete Datenstruktur �berlegen,
  * die eine laufzeiteffiziente Ausf�hrung der ISR erm�glicht.
  */
 
-struct Button {
-    const UChar portid;
-    const UChar bitid;
-    const UInt msg;
-} btn1, btn2;
+typedef struct {
+    const UChar * const port;
+    const UChar mask;
+    const TEvent msg;
+    UInt * const pcnt; // Hysterese Wert,
+} TButton;
+
+LOCAL struct {
+    UInt idx;
+    UInt cnt[BTNMAX];
+    const TButton *ptr;
+} button;
+
+LOCAL const TButton btn[BTNMAX] = {
+    {(UChar *)(&P1IN), BIT1, EVENT_BTN1, &button.cnt[0]},
+    {(UChar *)(&P1IN), BIT0, EVENT_BTN2, &button.cnt[1]}
+};
 
 // 10ms Schritte
 static const Char MUSTER_1[] = {8, 2, 0};
@@ -34,8 +47,6 @@ static const Char MUSTER_5[] = {2, 2, 2, 8, 0};
 static const Char MUSTER_6[] = {2, 2, 2, 2, 2, 8, 0};
 static const Char* P[] = {MUSTER_1, MUSTER_2, MUSTER_3, MUSTER_4, MUSTER_5, MUSTER_6};
 
-static UChar cnt_btn1;
-static UChar cnt_btn2;
 static UChar step_count;
 static UChar array_index;
 static UChar cnt_led;
@@ -56,8 +67,13 @@ GLOBAL Void set_blink_muster(UInt arg) {
 
 // Der Timer A0 ist bereits initialisiert
 GLOBAL Void TA0_Init(Void) {
-    cnt_btn1 = 0;
-    cnt_btn2 = 0;
+    button.idx = 0;
+    button.ptr = &btn[0];
+
+    for (UChar i=0; i LE BTNMAX-1; i++) {
+        button.cnt[i] = 0;
+    }
+
     step_count = TIMER_COUNT;
     array_index = 0;
     cnt_led = 0;
@@ -85,7 +101,7 @@ GLOBAL Void TA0_Init(Void) {
 
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt Void TA0_ISR(Void) {
-    //static volatile Int test = 0;
+    static volatile UChar test = 0;
     // Timer logic for 250ms step
     if (--step_count EQ 0) {
         //test = *(*(P + pattern_index) + array_index);
@@ -105,32 +121,25 @@ __interrupt Void TA0_ISR(Void) {
         }
     }
 
-    // Debouncing BTN1
-    if (TSTBIT(P1IN, BIT1)) {
-        if (cnt_btn1 LT MAX) {
-            if  (++cnt_btn1 EQ MAX) {
-                set_event(EVENT_BTN1);
+    // Debouncing
+    register UInt * const pcnt = button.ptr->pcnt;
+    if (TSTBIT(*button.ptr->port, button.ptr->mask)) {
+        if (*pcnt LT CNTMAX) {
+            if  (++(*pcnt) EQ CNTMAX) {
+                set_event(button.ptr->msg);
                 __low_power_mode_off_on_exit();
             }
         }
     } else {
-        if (cnt_btn1 GT 0) {
-            cnt_btn1--;
+        if (*pcnt GT 0) {
+            (*pcnt)--;
         }
     }
 
-    // Debouncing BTN2
-    if (TSTBIT(P1IN, BIT0)) {
-        if (cnt_btn2 LT MAX) {
-            if  (++cnt_btn2 EQ MAX) {
-                set_event(EVENT_BTN2);
-                __low_power_mode_off_on_exit();
-            }
-        }
-    } else {
-        if (cnt_btn2 GT 0) {
-            cnt_btn2--;
-        }
+    button.ptr++;
+    button.idx++;
+    if (button.idx GT BTNMAX-1) {
+        button.idx = 0;
+        button.ptr = &btn[0];
     }
 }
-
